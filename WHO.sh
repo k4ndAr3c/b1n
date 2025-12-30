6c093a74ef20edeaf6f1fcce51887805a73195d3
k4ndar3c <k4ndar3c@example.com>
2025-12-30 21:56:26 +0100

    add blacklist verif

--- WHO.sh
+++ WHO.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [[ -d ~/ip-blacklist-main ]] ; then
+	blip=$(grep -r $1 ~/ip-blacklist-main)
+	if [[ -z "$blip" ]] ; then
+		echo "$1 not found in blacklist"
+	else
+		echo "BLACKLISTED IP: $blip"
+	fi
+fi
 curl -s "https://api.proxynova.com/v1/geolocation/bulk?ip=$1" | jq
-echo
+echo "-----------------"
 ripdc.sh -t $1
 echo "-----------------"
 curl -s "http://ipinfo.io/$1"
